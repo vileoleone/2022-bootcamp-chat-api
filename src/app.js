44cbb2f4b9360e237144bdc1a3fd37bce57a5b3a
Victor Leone de Oliveira <vileoleone@gmail.com>
2022-11-08 22:50:55 -0300

    implemented MongoDB server initial configurations

--- src/app.js
+++ src/app.js
@@ -3,23 +3,42 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
-dotenv.config()
+//initial variables setup for back-end server
 
 const app = express()
 app.use(cors());
 app.use(express.json());
+dotenv.config()
+
+app.listen(5000, () => console.log("running in port 5000"))
+
+//setup for MongoDB server
+
 const MongoClient = new MongoClient(process.env.MONGO_URI)
 let db;
-
-app.listen(5000, () => "running in port 5000")
+let participants;
+let messages;
 
 MongoClient.connect().then(() => {
-    db = mongoClient.db("")
+    db = MongoClient.db("uolMockServer")
+    participants = db.collections("uolMockServerParticipants")
+    messages = db.collections("uolMockServerMessages")
 })
-    .catch(() => console.log())
+    .catch((err) => {
+    console.log(err)
+    })
 
-app.get("/", (req, res) => {
+// JSON for participants
+
+/* { name: 'João', lastStatus: 12313123 }  */
+
+// JSON for messages
+
+/* {from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'} */
+
+
+/* app.get("/", (req, res) => {
     db.collection("").toArray().find().then(() => {
 
     }).catch(() => console.log())
-})
+})  */
