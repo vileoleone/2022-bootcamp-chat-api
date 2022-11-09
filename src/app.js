a3607fd92345236e8eb22ba083da22c517f8d0ca
Victor Leone de Oliveira <vileoleone@gmail.com>
2022-11-09 00:03:49 -0300

    Implementação de post para rota /participants com conexão ao server

--- src/app.js
+++ src/app.js
@@ -2,43 +2,64 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+dotenv.config();
 
 //initial variables setup for back-end server
 
 const app = express()
 app.use(cors());
 app.use(express.json());
-dotenv.config()
-
-app.listen(5000, () => console.log("running in port 5000"))
 
 //setup for MongoDB server
 
-const MongoClient = new MongoClient(process.env.MONGO_URI)
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 let participants;
 let messages;
 
-MongoClient.connect().then(() => {
-    db = MongoClient.db("uolMockServer")
-    participants = db.collections("uolMockServerParticipants")
-    messages = db.collections("uolMockServerMessages")
+mongoClient.connect().then(() => {
+    db = mongoClient.db("uolMockServer")
+    participants = db.collection("uolMockServerParticipants")
+    messages = db.collection("uolMockServerMessages")
 })
     .catch((err) => {
-    console.log(err)
+        console.log(err)
     })
 
-// JSON for participants
-
-/* { name: 'João', lastStatus: 12313123 }  */
+/* // JSON for participants
+{ name: 'João', lastStatus: 12313123 }  
 
 // JSON for messages
+{from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'} 
+ */
 
-/* {from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'} */
 
+app.post("/participants", (req, res) => {
+
+    const { name } = req.body;
+    const toAdd = {
+        name,
+        lastStatus: Date.now()
+    }
+    if (!name || name === "" || typeof name !== "string") {
+        res.sendStatus(422)
+        return
+    }
+
+    participants.insertOne(toAdd).then(() => {
+        console.log(toAdd)
+        res.sendStatus(201);
+    })
+        .catch((err) => {
+            console.log(err)
+        })
+})
 
 /* app.get("/", (req, res) => {
     db.collection("").toArray().find().then(() => {
 
     }).catch(() => console.log())
 })  */
+
+
+app.listen(5000, () => console.log("running in port 5000"))
